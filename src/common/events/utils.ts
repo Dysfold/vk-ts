232e20640651193af33f1241b66ef1db72f22ae6
Ap3teus <asver37@gmail.com>
2020-07-27 19:46:25 +0300

    docs(ts): for event utils

--- src/common/events/utils.ts
+++ src/common/events/utils.ts
@@ -1,16 +1,17 @@
-import {
-  PlayerInteractEvent,
-  PlayerInteractAtEntityEvent,
-  PlayerInteractEntityEvent,
-} from 'org.bukkit.event.player';
+import { PlayerInteractEvent } from 'org.bukkit.event.player';
 import { Action } from 'org.bukkit.event.block';
 
+/**
+ * @returns whether or not the given `PlayerInteractEvent` involved a right click
+ */
 export function isRightClick(event: PlayerInteractEvent) {
   return [Action.RIGHT_CLICK_AIR, Action.RIGHT_CLICK_BLOCK].includes(
     event.action,
   );
 }
-
+/**
+ * @returns whether or not the given `PlayerInteractEvent` involved a left click
+ */
 export function isLeftClick(event: PlayerInteractEvent) {
   return [Action.LEFT_CLICK_AIR, Action.LEFT_CLICK_BLOCK].includes(
     event.action,
