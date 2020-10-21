c1c5f8e50ceb563bb3fcf01e24c4585c7d987427
Laetta <46357265+Laetta@users.noreply.github.com>
2020-10-21 21:42:31 +0300

    feat: add effects

--- src/mining/collapse.ts
+++ src/mining/collapse.ts
@@ -1,5 +1,5 @@
 import { Float } from 'java.lang';
-import { Location, Material } from 'org.bukkit';
+import { Location, Material, Sound, SoundCategory } from 'org.bukkit';
 import { Block } from 'org.bukkit.block';
 import { Player } from 'org.bukkit.entity';
 import { BlockBreakEvent } from 'org.bukkit.event.block';
@@ -14,8 +14,8 @@ const STONES = [
   Material.ANDESITE,
 ];
 
-const COLLAPSE_RADIUS = 7;
-const SUPPORT_RADIUS = 5;
+const COLLAPSE_RADIUS = 10;
+const SUPPORT_RADIUS = 6;
 const SUPPORTS = [
   Material.ACACIA_LOG,
   Material.BIRCH_LOG,
@@ -23,7 +23,6 @@ const SUPPORTS = [
   Material.JUNGLE_LOG,
   Material.OAK_LOG,
   Material.SPRUCE_LOG,
-
   Material.STRIPPED_ACACIA_LOG,
   Material.STRIPPED_BIRCH_LOG,
   Material.STRIPPED_DARK_OAK_LOG,
@@ -35,6 +34,9 @@ const SUPPORTS = [
 registerEvent(BlockBreakEvent, (event) => {
   if (event.player.itemInHand.type === Material.AIR) return;
   const block = event.block;
+
+  // We only collapse underground mines
+  if (block.lightFromSky !== 0) return;
   if (STONES.indexOf(block.type) === -1) return;
   if (Math.random() > COLLAPSE_CHECK_CHANCE) return;
 
@@ -43,9 +45,7 @@ registerEvent(BlockBreakEvent, (event) => {
 
   shake(event.player);
   playSound(location);
-  playSound(location);
-  playSound(location);
-  accident(location);
+  collapseCave(location);
 });
 
 function isSupported(location: Location) {
@@ -54,12 +54,13 @@ function isSupported(location: Location) {
   const y = location.y;
   const z = location.z;
   for (let i = x - SUPPORT_RADIUS; i < location.x + SUPPORT_RADIUS; i++) {
-    for (let j = z - SUPPORT_RADIUS; j < z + SUPPORT_RADIUS; j++) {
-      const material = world.getBlockAt(i, y, j).type;
-      if (SUPPORTS.indexOf(material) !== -1) {
-        // The mine was supported
-        server.broadcastMessage('supported');
-        return true;
+    for (let j = -1; j < 1; j++) {
+      for (let k = z - SUPPORT_RADIUS; k < z + SUPPORT_RADIUS; k++) {
+        const material = world.getBlockAt(i, y + j, k).type;
+        if (SUPPORTS.indexOf(material) !== -1) {
+          // The mine was supported
+          return true;
+        }
       }
     }
   }
@@ -67,7 +68,7 @@ function isSupported(location: Location) {
 }
 
 // Collapse large area of stone
-function accident(location: Location) {
+function collapseCave(location: Location) {
   const world = location.world;
   const x = location.x;
   const y = location.y;
@@ -105,6 +106,8 @@ function collapse(air: Block) {
         Material.COBBLESTONE,
         0,
       );
+      fallingBlock.setHurtEntities(true);
+      fallingBlock.setDropItem(false);
       fallingBlock.velocity = new Vector();
     }
   }
@@ -113,13 +116,10 @@ function collapse(air: Block) {
 async function shake(player: Player) {
   const directions = [new Vector(0.1, 0, 0.1), new Vector(-0.1, 0, -0.1)];
 
-  let count = 0;
-  const shaking = setInterval(() => {
-    player.setVelocity(player.velocity.add(directions[count % 2]));
-    if (count++ > 12) {
-      clearInterval(shaking);
-    }
-  }, 40);
+  for (let i = 0; i < 15; i++) {
+    player.setVelocity(player.velocity.add(directions[i % directions.length]));
+    await wait(10 * i, 'millis');
+  }
   return;
 }
 
@@ -127,11 +127,9 @@ function playSound(location: Location) {
   location.world.playSound(
     location,
     //'minecraft:item.totem.use',
-    //'minecraft:entity.wither.death',
-    'minecraft:block.chorus_flower.death',
-    2,
-    (new Float(0.3) as unknown) as number,
+    'minecraft:entity.wither.death',
+    //'minecraft:block.chorus_flower.death',
+    0.5,
+    (new Float(0.2) as unknown) as number,
   );
-  // GraalJS float handling is weird, need to explicitly "cast"
-  // And of course, TS types don't know that (yet)
 }
