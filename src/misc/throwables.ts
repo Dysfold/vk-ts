8c979b4ebef3229a358133c539191580b79ca2b1
Laetta <46357265+Laetta@users.noreply.github.com>
2020-11-22 23:30:18 +0200

    fix: remove velocity and replace getters and setters with new syntax

--- src/misc/throwables.ts
+++ src/misc/throwables.ts
@@ -1,14 +1,16 @@
-import { Material, Sound } from 'org.bukkit';
+import { Material, Sound, SoundCategory } from 'org.bukkit';
 import { EntityType, Snowball } from 'org.bukkit.entity';
 import { ProjectileHitEvent } from 'org.bukkit.event.entity';
 import { PlayerDropItemEvent } from 'org.bukkit.event.player';
 import { ItemStack } from 'org.bukkit.inventory';
+import { Vector } from 'org.bukkit.util';
 
 const VELOCITY_MULTIPLIER = 0.8;
 const THROW_SOUND = Sound.ENTITY_SNOWBALL_THROW;
 const HIT_SOUND = Sound.BLOCK_STONE_HIT;
+const ZERO_VECTOR = new Vector();
 
-const notThrowable: Material[] = [
+const NOT_THROWABLE: Material[] = [
   Material.SNOWBALL,
   Material.SPLASH_POTION,
   Material.HEART_OF_THE_SEA,
@@ -20,28 +22,38 @@ registerEvent(PlayerDropItemEvent, (event) => {
   const itemType = item.type;
 
   if (player.velocity.y < 0.08) return; // Return if player didn't jump
-  if (notThrowable.includes(itemType)) return;
+  if (NOT_THROWABLE.includes(itemType)) return;
 
   event.setCancelled(true);
-  item.setAmount(item.amount - 1);
+  item.amount--;
 
   const snowball = player.world.spawnEntity(
     player.location.add(0, 1.4, 0),
     EntityType.SNOWBALL,
   ) as Snowball;
 
-  snowball.setItem(new ItemStack(itemType, 1));
-  snowball.setVelocity(
-    player.eyeLocation.direction.multiply(VELOCITY_MULTIPLIER),
+  snowball.item = new ItemStack(itemType);
+  snowball.velocity = player.eyeLocation.direction.multiply(
+    VELOCITY_MULTIPLIER,
   );
-  player.world.playSound(player.location, THROW_SOUND, 0.5, 1);
+  player.world.playSound(
+    player.location,
+    THROW_SOUND,
+    SoundCategory.PLAYERS,
+    0.5,
+    1,
+  );
 });
 
 registerEvent(ProjectileHitEvent, (event) => {
   if (event.entity.type !== EntityType.SNOWBALL) return;
   const snowball = event.entity as Snowball;
-  if (notThrowable.includes(snowball.item.type)) return;
+  if (NOT_THROWABLE.includes(snowball.item.type)) return;
 
-  event.entity.world.dropItem(event.entity.location, snowball.item);
-  event.entity.world.playSound(event.entity.location, HIT_SOUND, 0.5, 1);
+  const drop = event.entity.world.dropItem(
+    event.entity.location,
+    snowball.item,
+  );
+  drop.velocity = ZERO_VECTOR; // Remove random velocity from the dropped item
+  drop.world.playSound(drop.location, HIT_SOUND, SoundCategory.PLAYERS, 0.5, 1);
 });
