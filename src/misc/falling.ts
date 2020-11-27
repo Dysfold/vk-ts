bfdd52f1a8eb680c969f788996501ff4983f5c12
Laetta <46357265+Laetta@users.noreply.github.com>
2020-11-27 10:20:55 +0200

    feat: custom fall damage

--- src/misc/falling.ts
+++ src/misc/falling.ts
@@ -2,10 +2,18 @@ import { LivingEntity } from 'org.bukkit.entity';
 import { EntityDamageEvent } from 'org.bukkit.event.entity';
 import { PotionEffect, PotionEffectType } from 'org.bukkit.potion';
 
-const slow = new PotionEffect(PotionEffectType.SLOW, 4 * 20, 2);
+const SLOW = new PotionEffect(PotionEffectType.SLOW, 4 * 20, 2);
+
+// Custom fall damage
+function fallDamage(old: number) {
+  return Math.ceil(old * old * 0.3);
+}
 
 registerEvent(EntityDamageEvent, (event) => {
   if (event.cause.toString() !== 'FALL') return;
-  const entity = event.getEntity() as LivingEntity;
-  entity?.addPotionEffect(slow);
+  if (!(event.entity instanceof LivingEntity)) return;
+
+  event.entity.addPotionEffect(SLOW);
+
+  event.damage = fallDamage(event.damage);
 });
