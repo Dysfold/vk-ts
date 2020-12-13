caf4f9b999d56914afb51dbf000de7fa7316af22
Laetta <46357265+Laetta@users.noreply.github.com>
2020-12-13 20:56:37 +0200

    fix: bottle itemmeta

--- src/hydration/bottles.ts
+++ src/hydration/bottles.ts
@@ -13,8 +13,61 @@ import {
 } from 'org.bukkit.inventory';
 import { PotionMeta } from 'org.bukkit.inventory.meta';
 import { PotionData, PotionType } from 'org.bukkit.potion';
+import { CustomItem } from '../common/items/CustomItem';
 
 const WATER_POTION_DATA = new PotionData(PotionType.WATER, false, false);
+
+const WineGlass = new CustomItem({
+  name: 'Viinilasi',
+  id: 1,
+  modelId: 1,
+  type: Material.POTION,
+});
+const WineGlassEmpty = new CustomItem({
+  name: 'Viinilasi',
+  id: 1,
+  modelId: 1,
+  type: Material.GLASS_BOTTLE,
+});
+
+const Mug = new CustomItem({
+  name: 'Muki',
+  id: 2,
+  modelId: 2,
+  type: Material.POTION,
+});
+const MugEmpty = new CustomItem({
+  name: 'Muki',
+  id: 2,
+  modelId: 2,
+  type: Material.GLASS_BOTTLE,
+});
+
+const Scoop = new CustomItem({
+  name: 'Kauha',
+  id: 3,
+  modelId: 3,
+  type: Material.POTION,
+});
+const ScoopEmpty = new CustomItem({
+  name: 'Kauha',
+  id: 3,
+  modelId: 3,
+  type: Material.GLASS_BOTTLE,
+});
+
+const BOTTLES = new Map<number, { full: ItemStack; empty: ItemStack }>([
+  [1, { full: WineGlass.create(), empty: WineGlassEmpty.create() }],
+  [2, { full: Mug.create(), empty: MugEmpty.create() }],
+  [3, { full: Scoop.create(), empty: ScoopEmpty.create() }],
+]);
+
+function getFullBottle(modelId: number) {
+  return BOTTLES.get(modelId)?.full || new ItemStack(Material.POTION);
+}
+function getEmptyBottle(modelId: number) {
+  return BOTTLES.get(modelId)?.empty || new ItemStack(Material.GLASS_BOTTLE);
+}
 
 /*
 Replace default bottle filling functionality
@@ -26,12 +79,9 @@ because those items would otherwise become normal bottles without custom model d
 // Fill a bottle
 registerEvent(PlayerInteractEvent, async (event) => {
   if (event.item?.type !== Material.GLASS_BOTTLE) return;
-  if (event.item.itemMeta.customModelData === 0) return; // Default glass bottle
-  if (
-    event.action !== Action.RIGHT_CLICK_AIR &&
-    event.action !== Action.RIGHT_CLICK_BLOCK
-  )
-    return;
+  if (!event.item.itemMeta.hasCustomModelData()) return; // Default glass bottle
+  const a = event.action;
+  if (a !== Action.RIGHT_CLICK_AIR && a !== Action.RIGHT_CLICK_BLOCK) return;
 
   // Didn't compile? Could this be used instead of setCancelled?
   //event.setUseItemInHand(Result.DENY)
@@ -72,12 +122,13 @@ registerEvent(PlayerInteractEvent, async (event) => {
   }
 
   if (bottleCanFill) {
-    const potion = new ItemStack(Material.POTION);
+    // Get corresponding customitem
+    const modelId = event.item.itemMeta.customModelData;
+    const potion = getFullBottle(modelId);
     const meta = potion.itemMeta;
+
     // Clear weird data from the potion (it would be pink)
     (meta as PotionMeta).setBasePotionData(WATER_POTION_DATA);
-    // Set the result match the custommodeldata of the item
-    meta.customModelData = event.item.itemMeta.customModelData;
     potion.itemMeta = meta;
 
     event.item.amount--;
@@ -85,16 +136,41 @@ registerEvent(PlayerInteractEvent, async (event) => {
   }
 });
 
+// Fill a cauldron
+registerEvent(PlayerInteractEvent, async (event) => {
+  if (event.item?.type !== Material.POTION) return;
+  if (!event.item.itemMeta.hasCustomModelData()) return; // Default glass bottle
+  const a = event.action;
+  if (a !== Action.RIGHT_CLICK_AIR && a !== Action.RIGHT_CLICK_BLOCK) return;
+
+  const block = event.clickedBlock;
+  if (block?.type !== Material.CAULDRON) return;
+  const levelled = block.blockData as Levelled;
+  if (levelled.level === levelled.maximumLevel) return;
+
+  // Player is filling a cauldron with custom bottle
+  const modelId = event.item.itemMeta.customModelData;
+  const bottle = getEmptyBottle(modelId);
+
+  // Wait 1 millis so we dont fire bottle fill event
+  await wait(1, 'millis');
+
+  const inventory = event.player.inventory as PlayerInventory;
+  if (event.hand === EquipmentSlot.HAND) {
+    inventory.itemInMainHand = bottle;
+  } else {
+    inventory.itemInOffHand = bottle;
+  }
+});
+
 // Drink a bottle
-const GLASS_BOTTLE = new ItemStack(Material.GLASS_BOTTLE);
 registerEvent(PlayerItemConsumeEvent, (event) => {
   if (event.item.type !== Material.POTION) return;
   if (!event.item.itemMeta.hasCustomModelData()) return; // Default bottle
 
-  const replacement = GLASS_BOTTLE;
-  const meta = replacement.itemMeta;
-  meta.customModelData = event.item.itemMeta.customModelData;
-  replacement.itemMeta = meta;
+  const modelId = event.item.itemMeta.customModelData;
+  const replacement = getEmptyBottle(modelId);
+
   event.replacement = replacement;
 });
 
