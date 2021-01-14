d419822ed2196bcfdebdd4fbb130ca901a8cd332
Laetta <46357265+Laetta@users.noreply.github.com>
2021-01-14 18:24:44 +0200

    style: add comments

--- src/misc/respack.ts
+++ src/misc/respack.ts
@@ -18,6 +18,7 @@ async function updateHash(announce = false) {
   const newHash = data.body as string;
 
   if (announce) {
+    // Check if the hash has changed, and then announce the new resource pack
     if (newHash && hash !== newHash) {
       Bukkit.server.broadcastMessage(
         '[Ilmoitus] Resurssipaketti on päivittynyt! Ota uusi resurssipaketti käyttöön komennolla /resurssipaketti',
@@ -51,10 +52,12 @@ function sendWarning(player: Player) {
   player.sendTitle('HUOMIO!', 'Resurssipaketti puuttuu', 1, 40, 1);
 }
 
+// Command to manually get the hash from the github
 registerCommand('updatehash', () => {
   updateHash(true);
 });
 
+// Command to download latest resource pack
 registerCommand(
   'resurssipaketti',
   (sender) => {
@@ -66,6 +69,9 @@ registerCommand(
   },
 );
 
+// Try to get new hash every x minutes.
+// Github API will only allow 60 requests per hour
+const INTERVAL_MINUTES = 1.5;
 setInterval(() => {
   updateHash(true);
-}, 2 * 60 * 1000);
+}, INTERVAL_MINUTES * 60 * 1000);
