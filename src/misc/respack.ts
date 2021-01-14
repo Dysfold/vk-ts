4e2082396a9a4b81722a41a4bf97021b6d30305d
Laetta <46357265+Laetta@users.noreply.github.com>
2021-01-14 19:35:21 +0200

    fix: add fallback

--- src/misc/respack.ts
+++ src/misc/respack.ts
@@ -4,6 +4,8 @@ import {
   PlayerJoinEvent,
   PlayerResourcePackStatusEvent,
 } from 'org.bukkit.event.player';
+import { Status } from 'org.bukkit.event.player.PlayerResourcePackStatusEvent';
+import { fail } from 'zora';
 
 const URL =
   'https://github.com/Laetta/respack/releases/download/latest/vk-respack.zip';
@@ -21,7 +23,7 @@ async function updateHash(announce = false) {
     // Check if the hash has changed, and then announce the new resource pack
     if (newHash && hash !== newHash) {
       Bukkit.server.broadcastMessage(
-        '[Ilmoitus] Resurssipaketti on päivittynyt! Ota uusi resurssipaketti käyttöön liittymällä peliin uudelleen',
+        '[Ilmoitus] Resurssipaketti on päivittynyt! Ota uusi resurssipaketti käyttöön liittymällä peliin uudelleen tai komennolla /resurssipaketti',
       );
     }
   }
@@ -34,22 +36,51 @@ registerEvent(PlayerJoinEvent, (event) => {
   downloadResourcePack(event.player);
 });
 
-registerEvent(PlayerResourcePackStatusEvent, (event) => {
-  if (event.status.toString() === 'DECLINED') {
-    sendWarning(event.player);
+const failedDownloads = new Set<Player>();
+
+registerEvent(PlayerResourcePackStatusEvent, async (event) => {
+  const player = event.player;
+
+  // Player declined the resource pack download
+  switch (event.status) {
+    case Status.DECLINED:
+      sendWarning(player);
+      break;
+
+    // Something went wrong while downloading. Try once more
+    case Status.FAILED_DOWNLOAD:
+      if (failedDownloads.has(player)) {
+        // Second time player failed to download
+        console.warn('Pelaajan ' + player.name + ' respack lataus epäonnistui');
+        sendWarning(player);
+        failedDownloads.delete(player);
+      }
+      // Try to download once more
+      // For some reason, when the resource pack (repo / hash) is updated, the first download fails
+      // This is the fallback and will download the resource pack
+      downloadResourcePack(player);
+      failedDownloads.add(player);
+      break;
+
+    case Status.SUCCESSFULLY_LOADED:
+      failedDownloads.delete(player);
+      break;
   }
 });
 
 function downloadResourcePack(player: Player) {
-  if (hash) player.setResourcePack(URL, hash);
-  else {
+  if (hash) {
+    player.setResourcePack(URL, hash);
+  } else {
+    // This might happen if the API breaks
     console.error('Resource pack hash ei löytynyt');
     player.resourcePack = URL;
   }
 }
 
 function sendWarning(player: Player) {
-  player.sendTitle('HUOMIO!', 'Resurssipaketti puuttuu', 1, 40, 1);
+  const seconds = player.isOp() ? 2 : 5;
+  player.sendTitle('HUOMIO!', 'Resurssipaketti puuttuu', 1, 20 * seconds, 1);
 }
 
 // Command to manually get the hash from the github
@@ -58,8 +89,6 @@ registerCommand('updatehash', () => {
 });
 
 // Command to download latest resource pack
-// FIXME: Player need to call this twice??
-/*
 registerCommand(
   'resurssipaketti',
   (sender) => {
@@ -70,7 +99,6 @@ registerCommand(
     executableBy: 'players',
   },
 );
-*/
 
 // Try to get new hash every x minutes.
 // Github API will only allow 60 requests per hour
