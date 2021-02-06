a477140598edbd7e772dcadb7e65aae9a114295f
Juffel <15192021+juffelis@users.noreply.github.com>
2021-02-06 02:40:46 +0200

    feat: initial timing and bleeding mechanism

--- src/misc/bleeding.ts
+++ src/misc/bleeding.ts
@@ -1,97 +1,145 @@
-import { Entity, EntityType } from 'org.bukkit.entity';
+import { Location, Material } from 'org.bukkit';
+import { BlockFace } from 'org.bukkit.block';
+import { Waterlogged } from 'org.bukkit.block.data';
+import { Damageable, EntityType } from 'org.bukkit.entity';
 import { EntityDamageEvent } from 'org.bukkit.event.entity';
+import { DamageCause } from 'org.bukkit.event.entity.EntityDamageEvent';
 
 /**
- * TODO: Minimum damage
- * TODO: Increase bleeding when more damage is caused
- * 
- * TODO: makeEntityBleed() is probably going to be used globally
+ * Creates a bleeding effect for given entity
  */
+class BleedTask {
+  private entity: Damageable;
+  public duration = 0.0;
+  public severity = 0.0;
 
-type TimerCallback = (time: number) => void; // Callback type for the timer
-
-/**
- * General purpose timer. Counts in seconds
- */
-class CountDownTimer {
-  duration: number = 0;
-
-  /**
-   * Timer status
-   */
-  isDead(): boolean {
-    return this.duration < 1;
-  }
-
-  /**
-   * Create a new timer instance
-   * @param duration timer duration in seconds
-   * @param callback callback function that provides remaining time
-   */
-  constructor(duration: number, callback: TimerCallback) {
+  constructor(
+    entity: Damageable,
+    duration: number,
+    severity: number,
+    callback: (error: Error) => void,
+  ) {
+    this.entity = entity;
     this.duration = duration;
+    this.severity = severity;
 
     const handle = setInterval(() => {
-      callback(this.duration);
-      if (this.duration-- < 1) clearInterval(handle);
+      try {
+        // Tick
+        this.bleed();
+
+        // Trigger timer to exit
+        if (this.duration-- < 1) throw undefined;
+      } catch (error) {
+        // Shutdown timer
+        clearInterval(handle);
+        callback(error);
+      }
     }, 1000);
   }
-}
 
-const timers = new Map<number, CountDownTimer>(); // store active timers
+  /**
+   * Spawn blood around given location
+   */
+  spawnBlood(location: Location, radius: number, chance: number) {
+    const origin = location.block;
 
-/**
- * Make entity bleed for given time
- * @param entity
- * @param duration in seconds
- * @param severity control bleeding amount
- */
-function makeEntityBleed(entity: Entity, duration: number, severity: number) {
-  if (timers.has(entity.entityId)) {
-    // Increase bleeding duration if entity is hurt again
-    timers.get(entity.entityId)!.duration += 2.0;
-  } else {
-    // Create new timer
-    timers.set(
-      entity.entityId,
-      new CountDownTimer(duration, (time) => {
-        bleed(time, entity, severity);
-      }),
+    for (let x = -radius; x <= radius; x++) {
+      for (let z = -radius; z <= radius; z++) {
+        // Randomly splutter blood
+        if (Math.random() > chance) continue;
+
+        // Compare against squared radius to check that point is inside circle
+        if (x * x + z * z > radius * radius) continue;
+
+        const block = origin.getRelative(x, 0.0, z);
+
+        // Check that block is valid
+        if (
+          !block.type.isAir() ||
+          !block.getRelative(BlockFace.UP).type.isAir() ||
+          !block.getRelative(BlockFace.DOWN).type.isOccluding()
+        )
+          return;
+
+        // Place blood
+        block.type = Material.DEAD_BUBBLE_CORAL_FAN;
+        const data = block.blockData as Waterlogged;
+        data.setWaterlogged(false);
+        block.blockData = data;
+      }
+    }
+  }
+
+  /**
+   * Handle tick
+   * 
+   * TODO: Decrease bloodloss; if (severity > min) severity-=0.1...
+   */
+  bleed() {
+    this.entity.sendMessage(
+      'You are bleeding for ' +
+        this.duration +
+        ' seconds at a rate of ' +
+        Math.fround(this.severity),
     );
 
-    // Check inactive timers in 10 second intervals
-    const handle = setInterval(() => {
-      if (timers.get(entity.entityId)!.isDead()) {
-        timers.delete(entity.entityId);
-        clearInterval(handle);
-      }
-    }, 1000 * 10);
+    this.spawnBlood(this.entity.location, 1.0, this.severity);
   }
 }
 
+// Store active tasks by entity id
+const tasks = new Map<number, BleedTask>();
+
 /**
- * Damage entity, play particles and throw blood everywhere
- * @param time time left in seconds
- * @param entity
- * @param severity
+ * Causes entity to bleed for a given time
+ * @param entity Damageable entity
+ * @param duration Duration in seconds
+ * @param severity Blood amount in the range of 0.0 to 1.0
  */
-function bleed(time: number, entity: Entity, severity: number) {
-  entity.sendMessage("You're bleeding for " + time + ' second(s)');
+function startEntityBleed(
+  entity: Damageable,
+  duration: number,
+  severity: number,
+) {
+  if (
+    tasks.has(entity.entityId) &&
+    tasks.get(entity.entityId)!.duration > 0.0
+  ) {
+    // Increase duration and severity if entity is damaged again while bleeding
+    const task = tasks.get(entity.entityId);
+    task!.duration += 1.0;
+    task!.severity += 0.1;
+  } else {
+    // Create new task for entity
+    tasks.set(
+      entity.entityId,
+      new BleedTask(entity, duration, severity, (error: Error) => {
+        if (error) {
+          log.error('[misc/bleeding] timer terminated with an error: ' + error);
+        }
+
+        // Remove task
+        tasks.delete(entity.entityId);
+      }),
+    );
+  }
 }
 
 registerEvent(EntityDamageEvent, (event) => {
   if (event.entityType == EntityType.PLAYER) {
-    makeEntityBleed(event.entity, 5.0, 1.0);
-
-    /*
     switch (event.cause) {
-      case DamageCause.CUSTOM ||
-        DamageCause.PROJECTILE ||
-        DamageCause.ENTITY_ATTACK:
+      case DamageCause.CUSTOM:
+      case DamageCause.PROJECTILE:
+      case DamageCause.ENTITY_ATTACK:
+        startEntityBleed(
+          event.entity as Damageable,
+          3.0,
+          event.finalDamage / 12.0,
+        );
         break;
       default:
         return;
     }
-    */
   }
 });
