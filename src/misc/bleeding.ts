65ad1b786bcc195f49c44010f277af81e373a3c0
Juffel <15192021+juffelis@users.noreply.github.com>
2021-02-15 00:32:36 +0200

    fix: separate bleeding start method, exceptionless control flow

--- src/misc/bleeding.ts
+++ src/misc/bleeding.ts
@@ -2,32 +2,22 @@ import { ParticleBuilder } from 'com.destroystokyo.paper';
 import { Location, Material, Particle } from 'org.bukkit';
 import { BlockFace } from 'org.bukkit.block';
 import { Waterlogged } from 'org.bukkit.block.data';
-import { Entity, EntityType, LivingEntity, Player } from 'org.bukkit.entity';
+import {
+  Entity,
+  EntityType,
+  HumanEntity,
+  LivingEntity,
+} from 'org.bukkit.entity';
 import { EntityDamageEvent, EntityDeathEvent } from 'org.bukkit.event.entity';
 import { DamageCause } from 'org.bukkit.event.entity.EntityDamageEvent';
 import { PotionEffect, PotionEffectType } from 'org.bukkit.potion';
-
-/**
- * Adds a bleeding effect for damageable entities by randomly generating blood puddles and splashes around entity
- *
- * Currently implemented features:
- *
- * - Damage amount and health determine how much the entity bleeds
- * - Injured entities bleed more violently when damaged again
- * - Bleeding exponentially decreases if not injured
- * - Armor, clothing etc... will affect bleeding amount
- * - It is now possible to determine entity health just by looking how much they bleed
- * - Blood loss causes nausea on players
- *
- * @author Juffel
- */
 
 const TIMER_DELAY = 1000; // timer delay in millis
 const BLOOD_MATERIAL = Material.DEAD_BUBBLE_CORAL_FAN; // blood material
 const PARTICLE_DATA = Material.REDSTONE_BLOCK.createBlockData(); // block data for the blood particle
 const PARTICLE_AMOUNT = 5; // amount of particles
 const TIME_STEP = 0.7; // control how long the bleeding continues. Higher is faster
-const COEFFICIENT = 0.55; // control how intensely the bleeding amount decreases at each step. Lower is faster
+const COEFFICIENT = 0.53; // control how intensely the bleeding amount decreases at each step. Lower is faster
 const NAUSEA_LIMIT = 0.79; // add nausea if blood loss amount exceeds this value
 const NAUSEA_DURATION = 15; // nausea duration in seconds
 
@@ -85,26 +75,25 @@ class BleedTask {
   amount = 0.0; // setpoint for the bleeding amount
 
   /**
-   * Create a bleeding effect for given entity
+   * Create new bleeding task
    * @param amount initial bleeding amount from the range of 0.0 to 1.0
-   * @param callback Exit callback
    */
-  constructor(
-    entity: Entity,
-    amount: number,
-    callback: (error: Error) => void,
-  ) {
+  constructor(entity: Entity, amount: number) {
     this.entity = entity;
     this.amount = amount;
+  }
 
+  /**
+   * Start bleeding task
+   * @param callback Exit callback
+   */
+  start(callback: () => void) {
     const handle = setInterval(() => {
-      try {
-        if (!this.tick()) throw undefined;
-
+      if (this.tick()) {
         this.time += TIME_STEP;
-      } catch (error) {
+      } else {
         clearInterval(handle);
-        callback(error);
+        callback();
       }
     }, TIMER_DELAY);
   }
@@ -197,7 +186,7 @@ class BleedTask {
    */
   addConfusion(duration: number) {
     if (this.entity.type == EntityType.PLAYER) {
-      const player = (this.entity as unknown) as Player;
+      const player = this.entity as HumanEntity;
       if (player.hasPotionEffect(PotionEffectType.CONFUSION)) return;
       player.addPotionEffect(
         new PotionEffect(PotionEffectType.CONFUSION, duration * 20, 1),
@@ -225,15 +214,16 @@ function setBleeding(entity: Entity, amount: number) {
   }
 
   // Create a new task
-  task = new BleedTask(entity, amount, (error: Error) => {
-    if (error) {
-      log.error('[misc/bleeding] timer terminated with an error: ' + error);
-    }
+  task = new BleedTask(entity, amount);
 
+  // Register task
+  tasks.set(entity.entityId, task);
+
+  // Start task
+  task.start(() => {
+    // Delete after finishing
     tasks.delete(entity.entityId);
   });
-
-  tasks.set(entity.entityId, task);
 }
 
 registerEvent(EntityDamageEvent, (event) => {
