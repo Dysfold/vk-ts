3ee50d028198075e2e204aa4ce1396b9aaed65e5
Raikas <fanipostiraikas@gmail.com>
2021-02-23 16:17:18 +0200

    Update hug.ts

--- src/misc/hug.ts
+++ src/misc/hug.ts
@@ -9,7 +9,7 @@ registerEvent(PlayerToggleSneakEvent, async (event) => {
     if(!event.isSneaking()) return;
     if(hugCooldowns.has(event.player)) return;
     const location = event.player.location.add(event.player.location.direction.multiply(0.7));
-    for(var player of location.getNearbyPlayers(0.5)) {
+    for(let player of location.getNearbyPlayers(0.5)) {
         if(player == event.player) continue;
         if(!player.isSneaking()) continue;
         hugCooldowns.add(player);
@@ -17,7 +17,7 @@ registerEvent(PlayerToggleSneakEvent, async (event) => {
         player.sendMessage(`§fPelaaja §7${event.player.name} §fhalaa sinua §4❤`)
         event.player.world.spawnParticle(Particle.HEART, event.player.eyeLocation.add(0,0.5,0), 1)
         player.world.spawnParticle(Particle.HEART, player.eyeLocation.add(0,0.5,0), 1)
-        for(var nearbyPlayer of event.player.location.getNearbyPlayers(10)) {
+        for(let nearbyPlayer of event.player.location.getNearbyPlayers(10)) {
             if(nearbyPlayer == event.player || nearbyPlayer == player) continue;
             nearbyPlayer.sendMessage(`§7${event.player.name} §fhalaa pelaajaa §7${player.name} §4❤`)
         }
