ee74dc78ba1f5164cb13f0d269c9a3056420a49a
Raikas <roni.aikas@gmail.com>
2021-02-26 14:21:11 +0200

    feat: disallow rope/weeping vine growing

---
+++ src/misc/rope.ts
@@ -0,0 +1,8 @@
+import { BlockSpreadEvent } from 'org.bukkit.event.block';
+import { Material } from 'org.bukkit';
+
+registerEvent(BlockSpreadEvent, async (event) => {
+    if(event.source.type == Material.WEEPING_VINES || event.source.type == Material.WEEPING_VINES_PLANT) {
+        event.setCancelled(true);
+    }
+})
