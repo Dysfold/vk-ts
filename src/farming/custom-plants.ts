c80afa46972a11a897e53fe9529ea71fffabfa45
Laetta <46357265+Laetta@users.noreply.github.com>
2021-02-28 00:05:09 +0200

    feat: add seeds

--- src/farming/custom-plants.ts
+++ src/farming/custom-plants.ts
@@ -1,58 +1,166 @@
-import { BlockSpreadEvent, BlockPlaceEvent } from 'org.bukkit.event.block';
-import { Material, Bukkit } from 'org.bukkit';
+import { Material } from 'org.bukkit';
 import { Ageable } from 'org.bukkit.block.data';
+import { BlockPlaceEvent, BlockSpreadEvent } from 'org.bukkit.event.block';
 import { ItemSpawnEvent } from 'org.bukkit.event.entity';
+import { ItemStack } from 'org.bukkit.inventory';
+import { CustomItem } from '../common/items/CustomItem';
 import { VkItem } from '../common/items/VkItem';
 
-const Plant = {
-  A: { id: 1 },
-  B: { id: 2 },
-  C: { id: 3 },
-  D: { id: 4 },
-  E: { id: 5 },
-  F: { id: 6 },
-  G: { id: 7 },
-  H: { id: 8 },
-  I: { id: 9 },
+interface Plant {
+  seeds: CustomItem<{}>;
+  minAge: number;
+  maxAge: number;
+  drops: ItemStack[];
+}
+
+const ExamplePlant1Seeds = new CustomItem({
+  id: 1,
+  modelId: 1,
+  type: VkItem.SEED,
+});
+const ExamplePlant1: Plant = {
+  seeds: ExamplePlant1Seeds,
+  minAge: 0,
+  maxAge: 2,
+  drops: [ExamplePlant1Seeds.create()],
 };
 
-const PLANTS = [
-  Plant.A, // 0
-  Plant.A, // 1
-  Plant.A, // 2
+const ExamplePlant2Seeds = new CustomItem({
+  id: 2,
+  modelId: 2,
+  type: VkItem.SEED,
+});
+const ExamplePlant2: Plant = {
+  seeds: ExamplePlant2Seeds,
+  minAge: 3,
+  maxAge: 5,
+  drops: [ExamplePlant2Seeds.create()],
+};
 
-  Plant.B, // 3
-  Plant.B, // 4
-  Plant.B, // 5
+const ExamplePlant3Seeds = new CustomItem({
+  id: 3,
+  modelId: 3,
+  type: VkItem.SEED,
+});
+const ExamplePlant3: Plant = {
+  seeds: ExamplePlant3Seeds,
+  minAge: 6,
+  maxAge: 8,
+  drops: [ExamplePlant3Seeds.create()],
+};
 
-  Plant.C, // 6
-  Plant.C, // 7
-  Plant.C, // 8
+const ExamplePlant4Seeds = new CustomItem({
+  id: 4,
+  modelId: 4,
+  type: VkItem.SEED,
+});
+const ExamplePlant4: Plant = {
+  seeds: ExamplePlant4Seeds,
+  minAge: 9,
+  maxAge: 11,
+  drops: [ExamplePlant4Seeds.create()],
+};
 
-  Plant.D, // 9
-  Plant.D, // 10
-  Plant.D, // 11
+const ExamplePlant5Seeds = new CustomItem({
+  id: 5,
+  modelId: 5,
+  type: VkItem.SEED,
+});
+const ExamplePlant5: Plant = {
+  seeds: ExamplePlant5Seeds,
+  minAge: 12,
+  maxAge: 14,
+  drops: [ExamplePlant5Seeds.create()],
+};
 
-  Plant.E, // 12
-  Plant.E, // 13
-  Plant.E, // 14
+const ExamplePlant6Seeds = new CustomItem({
+  id: 6,
+  modelId: 6,
+  type: VkItem.SEED,
+});
+const ExamplePlant6: Plant = {
+  seeds: ExamplePlant6Seeds,
+  minAge: 15,
+  maxAge: 17,
+  drops: [ExamplePlant6Seeds.create()],
+};
 
-  Plant.F, // 15
-  Plant.F, // 16
-  Plant.F, // 17
+const ExamplePlant7Seeds = new CustomItem({
+  id: 7,
+  modelId: 7,
+  type: VkItem.SEED,
+});
+const ExamplePlant7: Plant = {
+  seeds: ExamplePlant7Seeds,
+  minAge: 18,
+  maxAge: 20,
+  drops: [ExamplePlant7Seeds.create()],
+};
 
-  Plant.G, // 18
-  Plant.G, // 19
-  Plant.G, // 20
+const ExamplePlant8Seeds = new CustomItem({
+  id: 8,
+  modelId: 8,
+  type: VkItem.SEED,
+});
+const ExamplePlant8: Plant = {
+  seeds: ExamplePlant8Seeds,
+  minAge: 21,
+  maxAge: 25,
+  drops: [ExamplePlant8Seeds.create()],
+};
 
-  Plant.H, // 21
-  Plant.H, // 22
-  Plant.H, // 23
+const ExamplePlant9Seeds = new CustomItem({
+  id: 9,
+  modelId: 9,
+  type: VkItem.SEED,
+});
+const ExamplePlant9: Plant = {
+  seeds: ExamplePlant9Seeds,
+  minAge: 24,
+  maxAge: 25,
+  drops: [ExamplePlant9Seeds.create()],
+};
 
-  Plant.I, // 24
-  Plant.I, // 25
-];
+const AGES = new Map<number /* Age of the plant */, Plant /* Plant info */>([
+  [0, ExamplePlant1],
+  [1, ExamplePlant1],
+  [2, ExamplePlant1],
 
+  [3, ExamplePlant2],
+  [4, ExamplePlant2],
+  [5, ExamplePlant2],
+
+  [6, ExamplePlant3],
+  [7, ExamplePlant3],
+  [8, ExamplePlant3],
+
+  [9, ExamplePlant4],
+  [10, ExamplePlant4],
+  [11, ExamplePlant4],
+
+  [12, ExamplePlant5],
+  [13, ExamplePlant5],
+  [14, ExamplePlant5],
+
+  [15, ExamplePlant6],
+  [16, ExamplePlant6],
+  [17, ExamplePlant6],
+
+  [18, ExamplePlant7],
+  [19, ExamplePlant7],
+  [20, ExamplePlant7],
+
+  [21, ExamplePlant8],
+  [22, ExamplePlant8],
+  [23, ExamplePlant8],
+
+  [24, ExamplePlant9],
+  [25, ExamplePlant9],
+]);
+
+const PLANTS = new Set(AGES.values());
+
+// Custom growth for plants
 registerEvent(BlockSpreadEvent, (event) => {
   const block = event.source;
   if (block.type !== Material.TWISTING_VINES) return;
@@ -61,34 +169,52 @@ registerEvent(BlockSpreadEvent, (event) => {
   const oldAgeable = block.blockData as Ageable;
   const newAgeable = event.newState.blockData as Ageable;
 
-  const oldPlant = PLANTS[oldAgeable.age];
-  const newPlant = PLANTS[newAgeable.age];
+  const oldPlant = AGES.get(oldAgeable.age);
+  if (!oldPlant) return;
+  const newPlant = AGES.get(newAgeable.age);
+  if (!newPlant) return;
 
-  if (oldPlant.id === newPlant.id) {
+  // Plants are the same, if they have same minAge
+  if (oldPlant.minAge === newPlant.minAge) {
     oldAgeable.age++;
     block.blockData = oldAgeable;
   }
 });
 
+// Prevent regular twisting vines
 registerEvent(BlockPlaceEvent, (event) => {
   if (event.block.type === Material.TWISTING_VINES) {
     event.setCancelled(true);
   }
 });
 
+// Prevent twisting vines items from dropping
 registerEvent(ItemSpawnEvent, (event) => {
   if (event.entity?.itemStack?.type === Material.TWISTING_VINES)
     event.setCancelled(true);
 });
 
+// Place seeds on the fground
 registerEvent(BlockPlaceEvent, (event) => {
   if (event.itemInHand.type !== VkItem.SEED) return;
   const seed = event.itemInHand;
   if (!seed.itemMeta.hasCustomModelData()) return;
   const modelId = seed.itemMeta.customModelData;
   if (!modelId) return;
+
+  const minAge = getMinAge(event.itemInHand);
+  if (minAge === undefined) return;
+
   event.block.type = Material.TWISTING_VINES;
   const ageable = event.block.blockData as Ageable;
-  ageable.age = modelId - 1;
+  ageable.age = minAge;
   event.block.blockData = ageable;
 });
+
+function getMinAge(seeds: ItemStack) {
+  for (const plant of PLANTS) {
+    if (plant.seeds.check(seeds)) {
+      return plant.minAge;
+    }
+  }
+}
