382fd6a66febcd98552092dab667a68cd424fa4e
Raikas <roni.aikas@gmail.com>
2021-02-28 23:08:49 +0200

    fix: merge conflict

--- src/misc/rope.ts
+++ src/misc/rope.ts
@@ -2,7 +2,10 @@ import { BlockSpreadEvent } from 'org.bukkit.event.block';
 import { Material } from 'org.bukkit';
 
 registerEvent(BlockSpreadEvent, async (event) => {
-    if(event.source.type == Material.WEEPING_VINES || event.source.type == Material.WEEPING_VINES_PLANT) {
-        event.setCancelled(true);
-    }
-})
+  if (
+    event.source.type == Material.WEEPING_VINES ||
+    event.source.type == Material.WEEPING_VINES_PLANT
+  ) {
+    event.setCancelled(true);
+  }
+});
