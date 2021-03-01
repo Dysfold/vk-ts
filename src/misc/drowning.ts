6d6cc2833913c11bd8279bb4fc8242353235a483
Laetta <46357265+Laetta@users.noreply.github.com>
2021-03-01 17:56:08 +0200

    feat: basic structure

---
+++ src/misc/drowning.ts
@@ -0,0 +1,22 @@
+import { Bukkit } from 'org.bukkit';
+import { EntityType, Player } from 'org.bukkit.entity';
+import { EntityToggleSwimEvent } from 'org.bukkit.event.entity';
+
+registerEvent(EntityToggleSwimEvent, (event) => {
+  if (event.entityType !== EntityType.PLAYER) return;
+  const swimmer = (event.entity as unknown) as Player;
+
+  swimmer.sendMessage('JU');
+});
+
+setInterval(() => {
+  for (const player of Bukkit.server.onlinePlayers) {
+    if (player.isSwimming()) {
+      continue;
+    }
+  }
+}, 2000);
+
+function getWeight(swimmer: Player) {
+  const items = swimmer.inventory.contents.length;
+}
