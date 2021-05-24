c8c875994bd4c72c1bed80f86a8c8a1db258127f
Pete <65128229+PetePetelius@users.noreply.github.com>
2021-05-24 16:18:29 +0300

    style: changed 'ChanceTable' -> 'RandomTable', reformat code

--- src/mobs/piglin-trade.ts
+++ src/mobs/piglin-trade.ts
@@ -2,14 +2,14 @@ import { Material } from 'org.bukkit';
 import { Piglin } from 'org.bukkit.entity';
 import { EntityDropItemEvent, EntitySpawnEvent } from 'org.bukkit.event.entity';
 import { ItemStack } from 'org.bukkit.inventory';
-import { ChanceTable } from '../common/datas/ChanceTable';
+import { RandomTable } from '../common/datas/RandomTable';
 
-const trades = new ChanceTable<ItemStack>([
-  { value: new ItemStack(Material.YELLOW_WOOL, 1), rarity: 1 },
-  { value: new ItemStack(Material.BLACK_WOOL, 1), rarity: 5 },
-  { value: new ItemStack(Material.RED_WOOL, 1), rarity: 14 },
-  { value: new ItemStack(Material.BLUE_WOOL, 1), rarity: 30 },
-  { value: new ItemStack(Material.LIME_WOOL, 1), rarity: 50 },
+const trades = new RandomTable<ItemStack>([
+  { value: new ItemStack(Material.YELLOW_WOOL, 1), probability: 1 },
+  { value: new ItemStack(Material.BLACK_WOOL, 1), probability: 5 },
+  { value: new ItemStack(Material.RED_WOOL, 1), probability: 14 },
+  { value: new ItemStack(Material.BLUE_WOOL, 1), probability: 30 },
+  { value: new ItemStack(Material.LIME_WOOL, 1), probability: 50 },
 ]);
 
 // Prevent zombification of piglins in overworld.
@@ -23,7 +23,7 @@ registerEvent(EntitySpawnEvent, (event) => {
 registerEvent(EntityDropItemEvent, (event) => {
   if (!(event.entity instanceof Piglin)) return;
   const drop = event.itemDrop;
-  const tradeItem = trades.randomEntry();
+  const tradeItem = trades.randomEntry()?.clone();
   if (tradeItem) drop.itemStack = tradeItem;
   else drop.itemStack.amount = 0;
 });
