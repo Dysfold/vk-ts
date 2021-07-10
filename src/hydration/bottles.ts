94b93c332193658f370abc904da981f5d510387f
Laetta <46357265+Laetta@users.noreply.github.com>
2021-07-10 11:47:21 +0300

    fix: fix 1.17 cauldron bugs and refactor code

--- src/hydration/bottles.ts
+++ src/hydration/bottles.ts
@@ -1,7 +1,10 @@
-import { Material, Bukkit } from 'org.bukkit';
+import { text, translate } from 'craftjs-plugin/chat';
+import { Bukkit, Location, Material, SoundCategory } from 'org.bukkit';
+import { Block } from 'org.bukkit.block';
 import { Levelled, Waterlogged } from 'org.bukkit.block.data';
-import { Player, Entity } from 'org.bukkit.entity';
+import { Entity, Player } from 'org.bukkit.entity';
 import { Action, CauldronLevelChangeEvent } from 'org.bukkit.event.block';
+import { ChangeReason } from 'org.bukkit.event.block.CauldronLevelChangeEvent';
 import {
   PlayerInteractEvent,
   PlayerItemConsumeEvent,
@@ -13,16 +16,16 @@ import {
 } from 'org.bukkit.inventory';
 import { PotionMeta } from 'org.bukkit.inventory.meta';
 import { isRightClick } from '../common/helpers/click';
+import { giveItem } from '../common/helpers/inventory';
+import { centerOf } from '../common/helpers/locations';
 import { CustomItem } from '../common/items/CustomItem';
+import { addTranslation, t } from '../common/localization/localization';
 import {
   canFillCauldron,
   getPotionData,
   getWaterQuality,
   WaterQuality,
 } from './water-quality';
-import { ChangeReason } from 'org.bukkit.event.block.CauldronLevelChangeEvent';
-import { Block } from 'org.bukkit.block';
-import { translate } from 'craftjs-plugin/chat';
 
 export const WineGlass = new CustomItem({
   name: translate('vk.wine_glass'),
@@ -120,7 +123,7 @@ registerEvent(PlayerInteractEvent, async (event) => {
   let bottleCanFill = false;
   let waterQuality: WaterQuality | undefined = undefined;
 
-  const clickedBlock = event.clickedBlock;
+  let clickedBlock = event.clickedBlock;
   if (clickedBlock) {
     const blockData = clickedBlock.blockData;
     // Check if the block can be used to fill a bottle
@@ -137,8 +140,7 @@ registerEvent(PlayerInteractEvent, async (event) => {
         if (!checkCauldronEvent(clickedBlock, event.player, -1)) return;
 
         // Decrease the level of the cauldron
-        blockData.level--;
-        clickedBlock.blockData = blockData;
+        changeCauldronLevel(clickedBlock, -1);
 
         // Cauldrons provide normal water
         waterQuality = 'NORMAL';
@@ -155,10 +157,13 @@ registerEvent(PlayerInteractEvent, async (event) => {
     if (!lineOfSight) return;
     for (const block of lineOfSight) {
       if (block.type === Material.WATER) {
+        clickedBlock = block;
         bottleCanFill = true;
+        break;
       }
     }
   }
+  if (!clickedBlock) return;
 
   if (bottleCanFill) {
     // Get corresponding customitem
@@ -177,50 +182,82 @@ registerEvent(PlayerInteractEvent, async (event) => {
 
     event.item.amount--;
     giveItem(event.player, potion, event.hand);
+
+    playBottleFillSound(centerOf(clickedBlock));
   }
 });
 
 // Fill a cauldron
 registerEvent(PlayerInteractEvent, async (event) => {
   if (event.item?.type !== Material.POTION) return;
-
-  const a = event.action;
-  if (a !== Action.RIGHT_CLICK_AIR && a !== Action.RIGHT_CLICK_BLOCK) return;
+  if (event.action !== Action.RIGHT_CLICK_BLOCK) return;
 
   const block = event.clickedBlock;
-  if (block?.type !== Material.CAULDRON) return;
-  const levelled = block.blockData as Levelled;
-  if (levelled.level === levelled.maximumLevel) return;
+  if (!block) return;
+  const type = block.type;
+  if (type !== Material.CAULDRON && type !== Material.WATER_CAULDRON) return;
+
+  if (isCauldronFull(block)) return;
 
   event.setCancelled(true);
 
+  const player = event.player;
   if (!canFillCauldron(event.item)) {
-    event.player.sendActionBar(
-      'Voit täyttää padan vain tavallisella tai kirkkaalla vedellä',
+    player.sendActionBar(
+      text(t(player, 'bottles.wrong_water_to_fill_cauldron')),
     );
     return;
   }
 
   // Call cauldron level change event because the level changes
-  if (!checkCauldronEvent(block, event.player, 1)) return;
+  if (!checkCauldronEvent(block, player, 1)) return;
 
   // Fill the cauldron
-  levelled.level++;
-  block.blockData = levelled;
+  changeCauldronLevel(block, +1);
 
   // Player is filling a cauldron with custom bottle
   const bottle = getEmptyBottle(event.item);
 
+  playBottleEmptySound(centerOf(block));
+
   // Wait 1 millis so we dont fire bottle fill event
   await wait(1, 'millis');
 
-  const inventory = event.player.inventory as PlayerInventory;
+  const inventory = player.inventory as PlayerInventory;
   if (event.hand === EquipmentSlot.HAND) {
     inventory.itemInMainHand = bottle;
   } else {
     inventory.itemInOffHand = bottle;
   }
 });
+
+function changeCauldronLevel(block: Block, change: -1 | 1) {
+  const data = block.blockData;
+  if (data instanceof Levelled) {
+    const newLevel = Math.min(data.level + change, data.maximumLevel);
+
+    if (newLevel == 0) {
+      block.type = Material.CAULDRON;
+    } else {
+      data.level = newLevel;
+      block.blockData = data;
+    }
+    return;
+  }
+
+  if (change > 0) {
+    block.blockData = Material.WATER_CAULDRON.createBlockData();
+    (block.blockData as Levelled).level = 1;
+  }
+}
+
+function isCauldronFull(block: Block) {
+  const blockData = block.blockData;
+  if (blockData instanceof Levelled) {
+    if (blockData.level === blockData.maximumLevel) return true;
+  }
+  return false;
+}
 
 // Drink a bottle
 registerEvent(PlayerItemConsumeEvent, (event) => {
@@ -231,29 +268,6 @@ registerEvent(PlayerItemConsumeEvent, (event) => {
 
   event.replacement = replacement;
 });
-
-function giveItem(player: Player, item: ItemStack, hand: EquipmentSlot | null) {
-  if (item.type === Material.AIR) return;
-  // Prioritice players current hand
-  // OffHand
-  if (hand === EquipmentSlot.OFF_HAND) {
-    if ((player.inventory as PlayerInventory).itemInOffHand.type.isEmpty()) {
-      (player.inventory as PlayerInventory).itemInOffHand = item;
-      return;
-    }
-  }
-  // MainHand
-  else {
-    if ((player.inventory as PlayerInventory).itemInMainHand.type.isEmpty()) {
-      (player.inventory as PlayerInventory).itemInMainHand = item;
-      return;
-    }
-  }
-  const leftOver = player.inventory.addItem(item);
-  if (leftOver.size()) {
-    player.world.dropItem(player.location, leftOver.get(0));
-  }
-}
 
 /**
  * Calls CauldonLevelChangeEvent.
@@ -268,14 +282,22 @@ export function checkCauldronEvent(
   change: -1 | 1,
 ) {
   const data = block.blockData;
-  if (!(data instanceof Levelled)) return true; // Can't call event, not a cauldron
+  if (!(data instanceof Levelled)) {
+    return true; // Can't call event, not a cauldron
+  }
   const reason =
     change === 1 ? ChangeReason.BOTTLE_EMPTY : ChangeReason.BOTTLE_FILL;
 
   // Apply water level change to block state
-  data.level += change; // Change level
-  const state = block.state;
-  state.blockData = data; // Apply to state of cauldron
+  let state = block.state;
+  const newLevel = data.level + change;
+  if (newLevel == 0) {
+    block.type = Material.CAULDRON;
+    state = block.state;
+  } else {
+    data.level += change; // Change level
+    state.blockData = data; // Apply to state of cauldron
+  }
 
   const cauldronEvent = new CauldronLevelChangeEvent(
     block,
@@ -286,3 +308,28 @@ export function checkCauldronEvent(
   Bukkit.server.pluginManager.callEvent(cauldronEvent);
   return !cauldronEvent.isCancelled();
 }
+
+function playBottleFillSound(loc: Location) {
+  loc.world.playSound(
+    loc,
+    'minecraft:item.bottle.fill',
+    SoundCategory.PLAYERS,
+    1,
+    1,
+  );
+}
+
+function playBottleEmptySound(loc: Location) {
+  loc.world.playSound(
+    loc,
+    'minecraft:item.bottle.empty',
+    SoundCategory.PLAYERS,
+    1,
+    1,
+  );
+}
+
+addTranslation('bottles.wrong_water_to_fill_cauldron', {
+  fi_fi: 'Voit täyttää padan vain tavallisella tai kirkkaalla vedellä',
+  en_us: 'You can only fill the cauldron with  or normal or clear water',
+});
